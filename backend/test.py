8103da3325a4d7a94827ba620e216e62b31100fe
hugol <hugo.leyx-valade@efrei.net>
2025-06-26 16:53:58 +0200

    suppression du folder backend

--- backend/test.py
+++
@@ -1,9 +0,0 @@
-
-
-
-# Remplace chaque espace par un point-virgule, sauf si déjà un point-virgule
-with open('C:\\Users\\hugol\\Documents\\projet\\mastercamp\\MED-Metro-Efrei-Dodo-\\backend\\data\\metro.txt', 'r') as infile, open('output.txt', 'w') as outfile:
-    for line in infile:
-        parts = line.strip().split()
-        if parts:
-            outfile.write(';'.join(parts) + '\n')
